b5603c878071f7b15cc626379a837e0ec3c5f8fe
GOKUL.A.N <113367457+GOKUL-A-N@users.noreply.github.com>
2024-07-16 12:00:42 +0530

    Time: 1 ms (99.47%), Space: 41.8 MB (51.63%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.java
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -2,9 +2,7 @@ class Solution {
     public int scoreOfString(String s) {
         int score = 0;
         for(int i=0;i<s.length()-1;i++){
-            int elOne = (int)(s.charAt(i));
-            int elTwo = (int)(s.charAt(i+1));
-            score += Math.abs(elOne-elTwo);
+            score += Math.abs(((int)(s.charAt(i)))-((int)(s.charAt(i+1))));
         }
         return score;
     }
