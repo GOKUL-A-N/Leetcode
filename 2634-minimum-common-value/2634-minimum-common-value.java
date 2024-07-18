1c452e3eb2b43e099a1eb224cce77da1c1da19c8
GOKUL.A.N <113367457+GOKUL-A-N@users.noreply.github.com>
2024-07-18 10:40:15 +0530

    Time: 1 ms (94.22%), Space: 62.3 MB (82.3%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.java
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -1,9 +1,10 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        for(int n:nums1){
-            for(int i=0;i<nums2.length && nums2[i]<=n;i++){
-                if(nums2[i] == n) return n;
-            }
+        int l = 0 , r = 0;
+        while(l < nums1.length && r<nums2.length){
+            if(nums1[l] < nums2[r]) l++;
+            else if(nums1[l] > nums2[r]) r++;
+            else return nums1[l]; 
         }
         return -1;
     }
