196829e6a3b779d8de02ebe703f77f7f9f6a554e
GOKUL.A.N <113367457+GOKUL-A-N@users.noreply.github.com>
2024-08-06 14:57:58 +0530

    Time: 1 ms (99.88%), Space: 46 MB (42.55%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int num = 0;
-        for(int n:nums){
-            num ^= n;
+        for(int i=1;i<nums.length;i++){
+            nums[0] ^= nums[i];
         }
-        return num;
+        return nums[0];
     }
 }
